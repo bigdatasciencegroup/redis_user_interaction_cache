14c1bef9f4160bee0f4243ea0526bbdae85d1795
Jan Teichmann <jan.teichmann@gmail.com>
2019-11-13 10:49:01 +0000

    formatting

--- cache/cache.py
+++ cache/cache.py
@@ -8,20 +8,20 @@ from typing import Set, List
 
 
 LOGGER = logging.getLogger(__file__)
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PORT = os.environ['REDIS_PORT']
-REDIS_DB = os.environ['REDIS_DB']
-REDIS_TTL = datetime.timedelta(days=float(os.environ.get('REDIS_TTL', '7')))
+REDIS_HOST = os.environ["REDIS_HOST"]
+REDIS_PORT = os.environ["REDIS_PORT"]
+REDIS_DB = os.environ["REDIS_DB"]
+REDIS_TTL = datetime.timedelta(days=float(os.environ.get("REDIS_TTL", "7")))
 REDIS_TIME_RESOLUTION = datetime.timedelta(
-    days=float(os.environ.get('REDIS_RESOLUTION', '1')))
-
+    days=float(os.environ.get("REDIS_RESOLUTION", "1"))
+)
 
 
 class UserCache(object):
     def __init__(self, max_retry: int = 10):
         self.max_retry = max_retry
         self._redis = None
-        
+
     @property
     def redis(self) -> redis.Redis:
         """The Redis Python client connection object
@@ -35,7 +35,7 @@ class UserCache(object):
                     host=REDIS_HOST,
                     port=int(REDIS_PORT),
                     db=int(REDIS_DB),
-                    encoding='utf-8',
+                    encoding="utf-8",
                     decode_responses=True,
                 )
                 try:
@@ -44,8 +44,7 @@ class UserCache(object):
                     else:
                         time.sleep(1)
                 except Exception as e:
-                    LOGGER.error(
-                        "Cache connection failed with {}".format(e))
+                    LOGGER.error("Cache connection failed with {}".format(e))
                     time.sleep(1)
             if needs_client:
                 raise Exception("Could not establish connection to cache")
@@ -75,11 +74,10 @@ class UserCache(object):
         ) -> datetime.datetime(2019,2,1,12)
         """
         # how many secs have passed
-        nsecs = dt.hour*3600 + dt.minute*60 + dt.second + dt.microsecond*1e-6
+        nsecs = dt.hour * 3600 + dt.minute * 60 + dt.second + dt.microsecond * 1e-6
         delta = nsecs % res.total_seconds()
         return dt - datetime.timedelta(seconds=delta)
 
-    
     @staticmethod
     def unix_time(dt: datetime.datetime) -> int:
         """get seconds since epoch for datetime
@@ -100,8 +98,11 @@ class UserCache(object):
         result_set = set()
 
         for delta in range((REDIS_TTL.days // REDIS_TIME_RESOLUTION.days) + 1):
-            bucket = self.unix_time(self.floor_dt(
-                now - datetime.timedelta(days=delta), REDIS_TIME_RESOLUTION))
+            bucket = self.unix_time(
+                self.floor_dt(
+                    now - datetime.timedelta(days=delta), REDIS_TIME_RESOLUTION
+                )
+            )
             key = "{}:{}".format(userId, bucket)
             s = self.redis.smembers(key)
             if s:
@@ -120,8 +121,7 @@ class UserCache(object):
         :rtype: bool
         """
         bucket = self.unix_time(
-            self.floor_dt(datetime.datetime.utcnow(),
-            REDIS_TIME_RESOLUTION)
+            self.floor_dt(datetime.datetime.utcnow(), REDIS_TIME_RESOLUTION)
         )
         key = "{}:{}".format(userId, bucket)
         if type(interactionId) != list:
@@ -132,20 +132,20 @@ class UserCache(object):
             except Exception as e:
                 LOGGER.error(
                     "Failed to cache iteraction! {}:{} with error {}".format(
-                    key, interactionId, e)
+                        key, interactionId, e
+                    )
                 )
 
         try:
             self.redis.expire(key, REDIS_TTL)
         except Exception as e:
-            LOGGER.error(
-                "Failed to extend TTL of key: {} with error {}".format(
-                key, e)
-            )
+            LOGGER.error("Failed to extend TTL of key: {} with error {}".format(key, e))
 
         return True
 
-    async def user_cache(self, userId: str, interactionId: str, add_to_cache: bool) -> Set[str]:
+    async def user_cache(
+        self, userId: str, interactionId: str, add_to_cache: bool
+    ) -> Set[str]:
         """Async interaction with the user cache.
         
         :param userId: the corresponding user Id of an interaction
@@ -170,17 +170,16 @@ class UserCache(object):
             cache_response = set()
         try:
             if interactionId and add_to_cache:
-                _ = await asyncio.wait_for(
-                    add_to_cache_future, timeout=0.1
-                )
+                _ = await asyncio.wait_for(add_to_cache_future, timeout=0.1)
         except asyncio.TimeoutError:
             LOGGER.error(
-                "Failed to cache iteraction! {}:{}".format(
-                userId, interactionId)
+                "Failed to cache iteraction! {}:{}".format(userId, interactionId)
             )
         return cache_response
 
-    def __call__(self, userId: str, interactionId: str = None, add_to_cache: bool = False) -> List[str]:
+    def __call__(
+        self, userId: str, interactionId: str = None, add_to_cache: bool = False
+    ) -> List[str]:
         """Blocking call to the async user interaction cache.
         
         :param userId: the corresponding user Id of an interaction
